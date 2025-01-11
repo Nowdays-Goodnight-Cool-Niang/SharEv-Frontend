32cb24c3617b0808d13e4f34c7a80dfa8391d5da
Yujin Lee <yujinyll@naver.com>
2025-01-12 01:54:31 +0900

    탭 완성

---
+++ src/components/eventDetail/Tab.tsx
@@ -0,0 +1,20 @@
+interface TabProps {
+  text: string;
+  onClick: () => void;
+  isActive: boolean;
+}
+
+function Tab({ text, onClick, isActive }: TabProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`rounded-lg ${
+        isActive ? "text-blue-500 bg-white" : " text-gray-300"
+      } text-label-3 py-3 w-full `}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default Tab;
