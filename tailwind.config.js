5ea0da0b8f0ba37fffea34fcb4da1d9abb5282a9
chichoc <chichoc.dev@gmail.com>
2025-01-11 21:54:37 +0900

    style: 💄 색상 tailwindcss 변수화

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          30: '#FBFCFE',
+          50: '#F4F8FA',
+          70: '#E9EEF1',
+          100: '#CDD9E4',
+          200: '#90A2B7',
+          300: '#768797',
+          400: '#56687A',
+          500: '#41505B',
+        },
+        green: {
+          light: '#C1F57C',
+          dark: '#518E00',
+        },
+        blue: {
+          100: '#DCEFFF',
+          100: '#7FC3FF',
+          500: '#217DFF',
+        },
+        pink: '#FF8BF5',
+        orange: '#FF9852',
+        black: '#1E272E',
+        white: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 };
