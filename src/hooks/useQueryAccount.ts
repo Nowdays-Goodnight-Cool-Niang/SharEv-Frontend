39e5c34e32bd21945013bd7d22ce45ce4cd14beb
chichoc <chichoc.dev@gmail.com>
2025-01-12 10:06:49 +0900

    test: 💍 충돌해결

--- src/hooks/useQueryAccount.ts
+++ src/hooks/useQueryAccount.ts
@@ -1,7 +1,19 @@
-
 import { useQuery } from "@tanstack/react-query";
 import { accountAPI } from "../apis/accounts";
 import { IAccount } from "../types";
 
-export const useQueryAccount = (formAccount:IFormAccount) =>  useMutation({mutationFn:() => accountAPI.patchParticipantInfo(formAccount)});
-
+export const useQueryAccount = () => {
+  const {
+    data: account,
+    isLoading,
+    error,
+  } = useQuery<IAccount>({
+    queryKey: ["account"],
+    queryFn: accountAPI.getAccount,
+  });
+  return {
+    account,
+    isLoading,
+    error,
+  };
+};
