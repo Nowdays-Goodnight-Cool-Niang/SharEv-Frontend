901a05b28241f7a5593046a485777893f4994051
chichoc <chichoc.dev@gmail.com>
2025-03-26 22:36:22 +0900

    feat: 🎸 마이페이지 주요 요소 추가
    ✅ Closes: #24

--- src/components/mypage/MySection.tsx
+++ src/components/mypage/MySection.tsx
@@ -1,15 +1,32 @@
+import MyProfile from './MyProfile';
+
 function MySection() {
-  const profileFileds = {};
-  const settingFileds = [{ title: '문의하기' }, { title: '로그아웃' }];
+  const settingButtons = [{ title: '문의하기' }, { title: '로그아웃' }, { title: '탈퇴하기' }];
   const datas = [
-    { title: '프로필', field: profileFileds },
-    { title: '설정', field: settingFileds },
+    { title: '프로필', content: <MyProfile /> },
+    { title: '설정', buttons: settingButtons },
   ];
   return (
     <section className="mt-6">
       <>
         {datas.map((data) => (
-          <h2 className="text-title-2 mb-4 text-gray-400">{data.title}</h2>
+          <div className="mt-10" key={data.title}>
+            <h2 className="text-title-2 mb-4 text-gray-400">{data.title}</h2>
+            {data.buttons ? (
+              <div className="flex flex-col">
+                {data.buttons.map((button, idx) => (
+                  <button
+                    key={idx}
+                    className="text-body-3 rounded bg-gray-800 p-5 text-gray-200 hover:bg-gray-700"
+                  >
+                    {button.title}
+                  </button>
+                ))}
+              </div>
+            ) : (
+              data.content
+            )}
+          </div>
         ))}
       </>
     </section>
