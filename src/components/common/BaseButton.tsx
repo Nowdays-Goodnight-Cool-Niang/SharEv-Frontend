cc75570defdfc2a5c7a9272eeb775d0ec1bbab8c
chichoc <chichoc.dev@gmail.com>
2025-03-26 22:34:31 +0900

    style: 💄 disabled 스타일 추가
    disabled인 경우 다른 스타일 적용받지 않도록 수정 & disabled 조건 추가 (이름 & 이메일)
    
    ✅ Closes: #24

--- src/components/common/BaseButton.tsx
+++ src/components/common/BaseButton.tsx
@@ -4,17 +4,22 @@ interface IButtonPrimaryProps {
   isDisabled?: boolean;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
-function ButtonPrimary({ children, variant = 'default', isDisabled = false, onClick }: IButtonPrimaryProps) {
+function ButtonPrimary({
+  children,
+  variant = 'default',
+  isDisabled = false,
+  onClick,
+}: IButtonPrimaryProps) {
   const bgClasses = {
-    default: 'bg-orange-500 text-gray-50 hover:bg-gray-500',
+    default: 'bg-orange-500 text-gray-50 hover:bg-orange-700',
     kakao: 'bg-[#FEE500] text-[#000000]/85 hover:bg-[#eecc0d]',
   };
 
   return (
     <button
-      className={`${bgClasses[variant]} w-full text-button-1 p-4 rounded-lg transition-colors duration-400`}
+      className={`${bgClasses[variant]} text-button-1 duration-400 w-full rounded-lg p-4 transition-colors disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-25`}
       disabled={isDisabled}
-      onClick={isDisabled ? ()=> {} :  onClick}
+      onClick={onClick}
     >
       {children}
     </button>
