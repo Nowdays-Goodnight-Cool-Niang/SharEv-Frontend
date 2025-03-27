1937af6969805021bbbfd35064c8e358fee9d7bf
chichoc <chichoc.dev@gmail.com>
2025-03-27 18:05:53 +0900

    feat: 🎸 마이페이지 버튼별 클릭 동작 연결 및 문의하기 클릭시 토스트 띄우기
    ✅ Closes: #24

--- src/components/mypage/MySection.tsx
+++ src/components/mypage/MySection.tsx
@@ -1,7 +1,26 @@
 import MyProfile from './MyProfile';
+import toast from 'react-hot-toast';
 
 function MySection() {
-  const settingButtons = [{ title: '문의하기' }, { title: '로그아웃' }, { title: '탈퇴하기' }];
+  const handleInquiry = () => {
+    toast('준비 중입니다.', {
+      icon: '🙏🏻',
+    });
+  };
+
+  const handleLogout = () => {
+    // TODO: 로그아웃 API 호출
+  };
+
+  const handleAccountDeletion = () => {
+    // TODO: 탈퇴 (로직 논의 필요 - 재확인 or 별도 페이지)
+  };
+
+  const settingButtons = [
+    { title: '문의하기', onClick: handleInquiry },
+    { title: '로그아웃', onClick: handleLogout },
+    { title: '탈퇴하기', onClick: handleAccountDeletion },
+  ];
   const datas = [
     { title: '프로필', content: <MyProfile /> },
     { title: '설정', buttons: settingButtons },
@@ -17,9 +36,10 @@ function MySection() {
               {data.buttons.map((button, idx) => (
                 <button
                   key={idx}
+                  onClick={button.onClick}
                   className={`text-body-3 border border-gray-700 bg-gray-800 p-5 text-left text-gray-200 hover:bg-gray-700 ${
                     idx === 0 ? 'rounded-t' : ''
-                  } ${idx === data.buttons.length - 1 ? 'rounded-b border-none' : ''}`}
+                  } ${idx === data.buttons.length - 1 ? 'rounded-b border-none text-orange-500' : ''}`}
                 >
                   {button.title}
                 </button>
