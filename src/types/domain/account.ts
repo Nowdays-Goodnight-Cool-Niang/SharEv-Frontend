af1d775f2e585f3862faa5aabf432f505a8b3952
chichoc <chichoc.dev@gmail.com>
2025-08-01 21:50:47 +0900

    refactor: 💡 Account 계정 타입 통일(0801회의)
    ✅ Closes: #104

--- src/types/domain/account.ts
+++ src/types/domain/account.ts
@@ -1,14 +1,23 @@
-export interface ISocialLinks {
-  github?: string;
-  linkedIn?: string;
-  instagram?: string;
-}
-
-export interface IAccount {
+export interface IAccountApiResponse {
   id?: string;
   name: string;
   email: string;
   linkedinUrl?: string;
   githubUrl?: string;
   instagramUrl?: string;
+}
+
+export type IAccountUpdateRequest = Omit<IAccountApiResponse, 'id'>;
+
+export interface ISocialLinks {
+  githubUrl?: string;
+  linkedinUrl?: string;
+  instagramUrl?: string;
+}
+
+export interface IAccount {
+  id?: string;
+  name: string;
+  email: string;
+  socialLinks: ISocialLinks;
 }
